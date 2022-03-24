2860260a2136e5b57d72cceee3f98e422675e860
vaibhav rajuskar <vaibhavrajuskar54@gmail.com>
2022-03-24 21:17:05 +0530

    Added:Welcome to Employee Payroll service CRUD opeartion

---
+++ Main.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.Javasample;
+
+public class Main {
+	
+	    public static void main(String[] args) {
+	        System.out.println("Welcome to Employee Payroll Service");
+	    }
+	}
+
